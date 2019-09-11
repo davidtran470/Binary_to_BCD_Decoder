b091cf50332c81ec555855bf454b666b6f2dfd65
davidtran470 <49624953+davidtran470@users.noreply.github.com>
2019-09-10 19:27:07 -0700

    Update slowclock.v

--- slowclock.v
+++ slowclock.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:46:54 08/19/2019 
-// Design Name: 
-// Module Name:    slowclock 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module slowclock(
 	input clk,
 	input rst,
