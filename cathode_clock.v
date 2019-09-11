6a784ccb1ef9e7f7cd7eff314707effd2c7f6b08
davidtran470 <49624953+davidtran470@users.noreply.github.com>
2019-09-10 19:26:40 -0700

    Update cathode_clock.v

--- cathode_clock.v
+++ cathode_clock.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:07:41 08/19/2019 
-// Design Name: 
-// Module Name:    cathode_clock 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module cathode_clock(
 	input clk,
 	input rst,
